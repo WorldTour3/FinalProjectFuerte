03071c2369e9f44f71a5835441d423f5da9b0933
Flip Gill <gillpp@mail.uc.edu>
2025-04-27 00:07:34 -0400

    My part

--- FinalProjectFuerte/LeetCode02.cs
+++ FinalProjectFuerte/LeetCode02.cs
@@ -7,5 +7,39 @@ namespace FinalProjectFuerte
 {
     public class LeetCode02
     {
+        public int Candy(int[] ratings)
+        {
+            int n = ratings.Length;
+            int[] candies = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                candies[i] = 1;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (ratings[i] > ratings[i - 1])
+                {
+                    candies[i] = candies[i - 1] + 1;
+                }
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (ratings[i] > ratings[i + 1])
+                {
+                    candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+                }
+            }
+
+            int total = 0;
+            foreach (int candy in candies)
+            {
+                total += candy;
+            }
+
+            return total;
+        }
     }
 }
